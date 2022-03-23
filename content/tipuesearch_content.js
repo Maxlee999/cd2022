bcbb84ed2ad4cb5bc555509140ba8896cae903f1
Maxlee999 <Maxlee999@github.com>
2022-03-23 10:22:55 +0800

    pull request test

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 張程翔是虎尾馬斯克', 'tags': '', 'url': 'About.html'}]};
